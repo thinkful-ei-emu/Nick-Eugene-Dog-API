145e3192d006872edecdaa8374c724ab63866e48
geneggy <geneggy@gmail.com>
2019-06-11 08:56:38 -0700

    completed part 1

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImages(num) {
+  let url = 'https://dog.ceo/api/breeds/image/random/' + num;
+  console.log(url);
+  fetch(url)
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
+    .then(responseJson => console.log(responseJson))
+      //displayResults(responseJson))
+
+
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
-}
+// function verifyNum(num) {
+//   return(num >= 1 && num <= 50);
+// }
+
+// function displayResults(responseJson) {
+//   console.log(responseJson);
+//   //replace the existing image with the new one
+//   $('.results-img').replaceWith(
+//     `<img src="${responseJson.message}" class="results-img">`
+//   )
+//   //display the results section
+//   $('.results').removeClass('hidden');
+// }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    let numImgs = $('.numberInput').val();
+
+    getDogImages(numImgs);
+    console.log(getDogImages(numImgs));
   });
 }
 
